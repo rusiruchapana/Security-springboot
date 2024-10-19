779bf8f7a0683bdda761e9cd7a9b9e80462785e2
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-10-20 00:10:40 +0530

    create user & role entities.

---
+++ security-test/src/main/java/com/rusiruchapana/security/test/entity/User.java
@@ -0,0 +1,32 @@
+package com.rusiruchapana.security.test.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
