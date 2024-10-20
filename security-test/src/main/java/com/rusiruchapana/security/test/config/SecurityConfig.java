ccc512016b631d8a0c4be523da8533f4771cd507
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-10-20 20:12:06 +0530

    create the security filterchain bean class.

--- security-test/src/main/java/com/rusiruchapana/security/test/config/SecurityConfig.java
+++ security-test/src/main/java/com/rusiruchapana/security/test/config/SecurityConfig.java
@@ -1,10 +1,29 @@
 package com.rusiruchapana.security.test.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests->requests
+                        .requestMatchers("/register","/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
 }
